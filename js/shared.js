4b2de79b712a92de85c02c4cf22b9b78279637ea
Nickolas Andersen <nicksen782@gmail.com>
2023-06-22 19:05:13 -0400

    Improved commenting for the _APP.shared functions.

--- js/shared.js
+++ js/shared.js
@@ -1,6 +1,8 @@
 _APP.shared = {
     // Used for frame-count-based timing.
+    // Also can accept a callback to run when the timer finishes.
     genTimer: {
+        // Contains the timers. Each key in the object should be a gamestate1 and is an object of timers.
         timers:{},
 
         // Get a timer object.
@@ -82,7 +84,7 @@ _APP.shared = {
             // EXAMPLE USAGE:
             // _APP.shared.genTimer.create("timer1", 60, null);
             // _APP.shared.genTimer.create("timer1", 60, _APP.game.gs1);
-            // _APP.shared.genTimer.create("timer1", 60, _APP.game.gs1, function(){ console.log("I am the callback"); } );
+            // _APP.shared.genTimer.create("timer1", 60, _APP.game.gs1, function(){ console.log("I am the callback! I run when this timer finishes."); } );
 
             if(gamestate == undefined){ gamestate = _APP.game.gs1; }
             if(this.timers[gamestate] == undefined){ this.timers[gamestate] = {}; }
@@ -95,7 +97,7 @@ _APP.shared = {
             };
         },
 
-        // Resets a timer to it's starting state. (To reuse).
+        // Resets a timer to it's starting state. (Helpful when reusing a timer. You can also just create it again with the same values.)
         reset : function(name, gamestate){
             // EXAMPLE USAGE:
             // _APP.shared.genTimer.reset("timer1", null);
@@ -130,12 +132,12 @@ _APP.shared = {
                 return; 
             }
     
-            // Finish the timer. 
+            // Finish the timer.
             this.timers[gamestate][name] = {
                 finished  : true,
                 maxFrames : this.timers[gamestate][name].maxFrames,
                 frameCount: this.timers[gamestate][name].maxFrames,
-                callback: this.timers[gamestate][name].callback,
+                callback  : this.timers[gamestate][name].callback,
             };
         },
 
@@ -147,42 +149,53 @@ _APP.shared = {
             
             if(gamestate == undefined){ gamestate = _APP.game.gs1; }
 
+            // Cannot operate on a timer that does not exist!
             if(!this.timers[gamestate][name]){ 
                 console.error("ERROR: genTimer:check: This timer does not exist:", `name: '${name}', gamestate: '${gamestate}'`);
                 return; 
             }
 
-            // Return true if already finished.
+            // Return true if already finished. (This avoids running the callback function again.)
             if(this.timers[gamestate][name].finished){ return true; };
 
             // Check and update the timer. 
-            if(
-                this.timers[gamestate][name].frameCount >= this.timers[gamestate][name].maxFrames && 
-                !this.timers[gamestate][name].finished
-            ){
+            if( this.timers[gamestate][name].frameCount >= this.timers[gamestate][name].maxFrames ){
+                // Set the finished flag.
                 this.timers[gamestate][name].finished = true;
             }
             else{
-                // Increment by 1.
+                // Increment the frameCount by 1.
                 this.timers[gamestate][name].frameCount += 1;
             }
 
-            // If this is now finished and a callback was assigned...
+            // If this timer is now marked as finished check if there is a callback function assigned to it.
             if(this.timers[gamestate][name].finished){ 
-                // console.log( "FINISHED:", _APP.shared.genTimer.get(name), "RUNNING CALLBACK." );
+                // If there is a callback function assigned then run the callback function.
                 if(this.timers[gamestate][name].callback){
                     this.timers[gamestate][name].callback();
                 };
             };
 
+            // Return the state of the timer (If finished then true, otherwise, false.)
             return this.timers[gamestate][name].finished;
         },
     },
 
     // A queue of functions intended to be run once each and sequentially.
+    // NOTES: 
+    // runNext: The next func does not wait for the prev func to finish. 
+    // For example, when queuing a reverse and a skip then the skip would nearly immediately override the reverse.
+    // If you need to run functions sequentially then you can nest genTimers instead. The callback function runs AFTER the timer completes.
     funcQueue: {
-        funcs: {},
+        // Contains the functions. Each key in the object should be a gamestate1 and is an array of functions.
+        funcs: {}, 
+
+        // Adds a new function the the funcQueue.
         create: function(name, gamestate, funcObj){
+            // EXAMPLE USAGE:
+            // _APP.shared.funcQueue.create("timer1", null, ()=>{});
+            // _APP.shared.funcQueue.create("timer1", _APP.game.gs1, ()=>{ console.log("I am the callback! I run in sequence to any other functions in the funcQueue."); });
+
             // Make sure that a function was specified.
             if(!funcObj.func){ 
                 console.error("ERROR: funcQueue:get: A function was not specified.", `name: '${name}', gamestate: '${gamestate}'`);
@@ -202,7 +215,13 @@ _APP.shared = {
             this.funcs[gamestate].push(newEntry);
             return newEntry;
         },
+        
+        // Shifts off the first function from the funcs[gamestate] array and runs it.
         runNext: function(gamestate){
+            // EXAMPLE USAGE:
+            // _APP.shared.funcQueue.runNext(null);
+            // _APP.shared.funcQueue.runNext(_APP.game.gs1);
+
             // Add the gamestate key to the funcs object if it is missing.
             if(this.funcs[gamestate] == undefined){ this.funcs[gamestate] = []; }
 
@@ -218,7 +237,13 @@ _APP.shared = {
             // The funcObj is no longer part of this.funcs[gamestate].
             return false;
         },
+
+        // Clears the funcs[gamestate] array.
         clearQueue: function(gamestate){
+            // EXAMPLE USAGE:
+            // _APP.shared.funcQueue.clearQueue(null);
+            // _APP.shared.funcQueue.clearQueue(_APP.game.gs1);
+
             // Add the gamestate key to the funcs object if it is missing.
             if(this.funcs[gamestate] == undefined){ this.funcs[gamestate] = []; }
 
@@ -227,7 +252,17 @@ _APP.shared = {
         },
     },
 
+    // Utility function to retrieve gamestates for P1 and P2.
     getAllGamepadStates: function(){
+        // ANY is a combination of P1 and P2 states.
+        // ANY_bool is simply a true/false for the combination of P1 and P2 states.
+        // The button states are "held", "press", "release".
+        // Button names: 
+        //   BTN_SR   , BTN_SL                       // Shoulder buttons (top.)
+        //   BTN_X    , BTN_A     , BTN_Y, BTN_B     // Face buttons (right side.)
+        //   BTN_RIGHT, BTN_LEFT  , BTN_DOWN, BTN_UP // Directional buttons (left side.)
+        //   BTN_START, BTN_SELECT                   // Center buttons (center.)
+        
         // Get the gamepad states for p1 and p2.
         let gpInput = {
             "P1": _INPUT.util.stateByteToObj2("p1"),
@@ -236,34 +271,52 @@ _APP.shared = {
 
         // Create a new key "ANY" that can be checked for any buttons pressed by either player.
         gpInput["ANY"] = {
-            "held": {},
-            "press": {},
+            "held"   : {},
+            "press"  : {},
             "release": {}
         };
+
+        // Create a new key "ANY_bool" that only provides true/false for a button state.
         gpInput["ANY_bool"] = {
             "held"   : false,
             "press"  : false,
             "release": false
         };
+
+        // Available button states.
         const states = ["held", "press", "release"];
+
+        // Available buttons. 
         const buttons = _INPUT.consts.buttons;
 
+        // Populate "ANY" and "ANY_bool"...
         // Iterate through each state ("held", "press", "release").
         for (let i = 0; i < states.length; i++) {
+            // Get the state name.
             const state = states[i];
 
-            // Iterate through each button.
+            // Iterate through each button for this state.
             for (let j = 0; j < buttons.length; j++) {
+                // Get the button name. 
                 const button = buttons[j];
 
                 // Set the value in "ANY" to true if either "P1" or "P2" has that property set to true.
                 gpInput["ANY"][state][button] = gpInput["P1"][state][button] || gpInput["P2"][state][button];
-                if(!gpInput["ANY_bool"][state] && gpInput["P1"][state][button] || gpInput["P2"][state][button]){
+
+                // Any button pressed on a state should update ANY_bool to true for that button state.
+                if(
+                    // Don't update if already set.
+                    !gpInput["ANY_bool"][state] && 
+                    // If either P1 or P2 for the state and button are true.
+                    gpInput["P1"][state][button] || gpInput["P2"][state][button]
+                ){
+                    // Set ANY_bool's value for the state.
                     gpInput["ANY_bool"][state] = true;
                 }
             }
         }
 
+        // Return the completed gamepad input.
         return gpInput;
     },
 };
